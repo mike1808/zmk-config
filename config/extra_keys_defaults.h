f49a06b693eed23845b8fbcd5f5ffcfaf78d3924
Mikael Manukyan <arm.localhost@gmail.com>
2022-11-24 00:40:21 -0800

    Refactor common keymap

---
+++ config/extra_keys_defaults.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#ifndef XTR_DEF_LR
+#define XTR_DEF_LR &none
+#endif
+
+#ifndef XTR_DEF_RR
+#define XTR_DEF_RR &none
+#endif
+
+#ifndef XTR_SYM_LR
+#define XTR_SYM_LR &none
+#endif
+
+#ifndef XTR_SYM_RR
+#define XTR_SYM_RR &none
+#endif
+
+#ifndef XTR_NUM_LR
+#define XTR_NUM_LR &none
+#endif
+
+#ifndef XTR_NUM_RR
+#define XTR_NUM_RR &none
+#endif
+
+#ifndef XTR_ADJ_LR
+#define XTR_ADJ_LR &none
+#endif
+
+#ifndef XTR_ADJ_RR
+#define XTR_ADJ_RR &none
+#endif
+
+#ifndef XTR_DEF_LT
+#define XTR_DEF_LT &none
+#endif
+
+#ifndef XTR_DEF_RT
+#define XTR_DEF_RT &none
+#endif
+
+#ifndef XTR_SYM_LT
+#define XTR_SYM_LT &none
+#endif
+
+#ifndef XTR_SYM_RT
+#define XTR_SYM_RT &none
+#endif
+
+#ifndef XTR_NUM_LT
+#define XTR_NUM_LT &none
+#endif
+
+#ifndef XTR_NUM_RT
+#define XTR_NUM_RT &none
+#endif
+
+#ifndef XTR_ADJ_LT
+#define XTR_ADJ_LT &none
+#endif
+
+#ifndef XTR_ADJ_RT
+#define XTR_ADJ_RT &none
+#endif
