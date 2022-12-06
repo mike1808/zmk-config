840232ebb07883907a00da17064e771af4e0d98f
Mikael Manukyan <arm.localhost@gmail.com>
2022-12-05 20:41:12 -0800

    Fix base layout for Corne

--- config/extra_keys_defaults.h
+++ config/extra_keys_defaults.h
@@ -1,65 +1,65 @@
 #pragma once
 
 #ifndef XTR_DEF_LR
-#define XTR_DEF_LR &none
+#define XTR_DEF_LR
 #endif
 
 #ifndef XTR_DEF_RR
-#define XTR_DEF_RR &none
+#define XTR_DEF_RR
 #endif
 
 #ifndef XTR_SYM_LR
-#define XTR_SYM_LR &none
+#define XTR_SYM_LR
 #endif
 
 #ifndef XTR_SYM_RR
-#define XTR_SYM_RR &none
+#define XTR_SYM_RR
 #endif
 
 #ifndef XTR_NUM_LR
-#define XTR_NUM_LR &none
+#define XTR_NUM_LR
 #endif
 
 #ifndef XTR_NUM_RR
-#define XTR_NUM_RR &none
+#define XTR_NUM_RR
 #endif
 
 #ifndef XTR_ADJ_LR
-#define XTR_ADJ_LR &none
+#define XTR_ADJ_LR
 #endif
 
 #ifndef XTR_ADJ_RR
-#define XTR_ADJ_RR &none
+#define XTR_ADJ_RR
 #endif
 
 #ifndef XTR_DEF_LT
-#define XTR_DEF_LT &none
+#define XTR_DEF_LT
 #endif
 
 #ifndef XTR_DEF_RT
-#define XTR_DEF_RT &none
+#define XTR_DEF_RT
 #endif
 
 #ifndef XTR_SYM_LT
-#define XTR_SYM_LT &none
+#define XTR_SYM_LT
 #endif
 
 #ifndef XTR_SYM_RT
-#define XTR_SYM_RT &none
+#define XTR_SYM_RT
 #endif
 
 #ifndef XTR_NUM_LT
-#define XTR_NUM_LT &none
+#define XTR_NUM_LT
 #endif
 
 #ifndef XTR_NUM_RT
-#define XTR_NUM_RT &none
+#define XTR_NUM_RT
 #endif
 
 #ifndef XTR_ADJ_LT
-#define XTR_ADJ_LT &none
+#define XTR_ADJ_LT
 #endif
 
 #ifndef XTR_ADJ_RT
-#define XTR_ADJ_RT &none
+#define XTR_ADJ_RT
 #endif
